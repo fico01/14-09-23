9062e38de756625fb6a3e54c2bf812f6bbee4a3a
Fico <112430242+fico01@users.noreply.github.com>
2023-09-14 11:19:49 -0300

    if.js

---
+++ if.js
@@ -0,0 +1,10 @@
+var senhaCorreta = "slaalgumacoisa";
+
+
+var senhaDoUsuario = "senhaIncorreta"; 
+
+if (senhaDoUsuario === senhaCorreta) {
+  console.log("Acesso permitido! Bem-vindo ao aplicativo.");
+} else {
+  console.log("Senha incorreta. Tente novamente.");
+}
